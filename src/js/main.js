25f3075bd752dc258b41e29bbf255ecdd1a54bb9
Kirill Barkov <freedomdragon777@gmail.com>
2025-04-13 19:47:58 +0400

    Добавлены зависимости для @vercel/speed-insights и инициализация в основном скрипте приложения.

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,18 @@
 // Импорты стилей
 import '../styles/main.scss';
 
+// Импорт SpeedInsights
+import { SpeedInsights } from "@vercel/speed-insights";
+
 // Импорты модулей
 // import './modules/example';
 
 // Основной код
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Приложение запущено!');
+  
+  // Инициализация SpeedInsights
+  new SpeedInsights();
   
   // Здесь будет основной код приложения
 }); 
