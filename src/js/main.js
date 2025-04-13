5b0587786e25a690d3ec86e80f29733a5949b4ea
Kirill Barkov <freedomdragon777@gmail.com>
2025-04-13 19:43:56 +0400

    Initial Commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// Импорты стилей
+import '../styles/main.scss';
+
+// Импорты модулей
+// import './modules/example';
+
+// Основной код
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Приложение запущено!');
+  
+  // Здесь будет основной код приложения
+}); 
